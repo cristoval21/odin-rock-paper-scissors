39e0b860476938868182317b2c83679a08263dca
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-01-15 08:00:14 +0700

    Fix getComputer() function return value

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function getComputerChoice() {
-  return Math.floor((Math.random() * 3)) + 1
+  let randomNumber = Math.floor((Math.random() * 3)) + 1
+
+  switch (randomNumber) {
+    case 1:
+      return "rock"
+    case 2:
+      return "paper"
+    case 3:
+      return "scissors"
+  }
 }
+
+console.log(getComputerChoice())
