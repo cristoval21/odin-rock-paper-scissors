a52d7221288701b339c728cde7f09937bafbdf6d
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-01-22 13:17:37 +0700

    Add UI

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+  const computerSelection = getComputerChoice()
+
   if (playerSelection === computerSelection) {
     playerScore++
     computerScore++
+    
     return "tie"
   } else if (
     (playerSelection === "rock" && computerSelection === "paper") ||
@@ -26,9 +29,13 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "scissors" && computerSelection === "rock")
   ) {
     computerScore++
+    roundsPlayed++
+
     return "lose"
   } else {
     playerScore++
+    roundsPlayed++
+
     return "win"
   }
 }
@@ -54,17 +61,67 @@ function game() {
   return result
 }
 
-while (roundsPlayed < 5) {
-  const roundResult = game()
+// while (roundsPlayed < 5) {
+//   const roundResult = game()
 
-  if (roundResult != "tie") roundsPlayed++
-}
+//   if (roundResult != "tie") roundsPlayed++
+// }
 
-console.log(`Final score: ${playerScore} - ${computerScore}`)
-if (playerScore > computerScore) {
-  console.log("You beat the computer!")
-} else if (playerScore < computerScore) {
-  console.log("The computer beat you!")
-} else {
-  console.log("The game was a tie!")
-}
+// console.log(`Final score: ${playerScore} - ${computerScore}`)
+// if (playerScore > computerScore) {
+//   console.log("You beat the computer!")
+// } else if (playerScore < computerScore) {
+//   console.log("The computer beat you!")
+// } else {
+//   console.log("The game was a tie!")
+// }
+
+const buttonWrapper = document.querySelector(".button-wrapper")
+const playerScoreboard = document.querySelector(".player-score")
+const computerScoreboard = document.querySelector(".computer-score")
+const winner = document.querySelector(".winner")
+buttonWrapper.addEventListener("click", (e) => {
+  const target = e.target
+  let result = ""
+
+  switch (target.id) {
+    case "btn-rock":
+      result = playRound("rock")
+      break;
+    case "btn-paper":
+      result = playRound("paper")
+      break;
+    case "btn-scissors":
+      result = playRound("scissors")
+      break;
+  }
+  
+  // Update scoreboard
+  playerScoreboard.textContent = playerScore
+  computerScoreboard.textContent = computerScore
+
+  if (playerScore >= 5 || computerScore >= 5) {
+    for (const button of buttonWrapper.children) {
+      button.disabled = true
+    }
+
+    if (playerScore < computerScore) {
+      winner.textContent = "You lost the game :("
+    } else {
+      winner.textContent = "Congratulations, you beat the computer!"
+    }
+  } else {
+    switch (result) {
+      case "tie":
+        winner.textContent = "The round was a tie!"
+        break
+      case "lose":
+        winner.textContent = "You lost the round!"
+        break
+      case "win":
+        winner.textContent = "You won the round!"
+        break
+    }
+  }
+
+})
